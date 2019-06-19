b519ac336494e58898735f72423f140d08c03117
Diego <testingwebcourse@gmail.cm>
2019-06-19 18:17:11 -0500

    Apuntes De Clase
    Log para guardar algunos apuntes de clase

---
+++ apuntesdeclase.js
@@ -0,0 +1,65 @@
+//Operaciones basicas
+
+var numberOne =1
+var numberTwo =2
+var double =1.20000000001
+var array =["h",3,1.058]
+var obj ={"head":5,"array":array}
+var sumar = numberOne + numberTwo
+var restar = numberOne - numberTwo
+var dividir = numberOne / numberTwo
+var multiplicar = numberOne * numberTwo
+
+//double ="este es mi mensaje para doble"
+console.log("la suma de las variables es:", sumar)
+console.log("la resta de las variables es:", restar)
+console.log("la division de las variables es:", dividir)
+console.log("la multiplicióm de las variables es:", multiplicar)
+console.log("double", double)
+console.log("array",array)
+console.log("objeto",obj)
+
+//Funciones JS
+//console.log()
+//typeof() --para saber el tipo de dato que estamos usando
+//parseInt(12.135)pasar de un flotante a un entero
+//alert() genera alertas en el browser
+//string() cambia de numero a string
+
+//Operadores Matemáticos
+//Especializados: https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/Math
+//Dividir %
+//Potencia **
+//Incremento var x = 5; x++
+//Reducir incremento x--
+//Operadores de comparacion == igual a igual --si los dos valores tienen el mismo dato
+//Igual en valor y tipo === 
+//!= Diferente de
+//numero !== "numero" no es igual en valor y tipo
+//mayor que >
+//Menor que <
+//>= mayor o igual
+//<= menor o igual
+
+//Operador ternario: condicion ? expr1:expr2 --es un if en una sola linea, antes del signo de interrogacion
+//van las operaciones
+
+//Operadores lógicos
+//&& AND ejemplo 1&&1
+//|| OR  1||0
+//! negacion !true
+
+//Sentencias condicionales
+// If, else, elseif
+//
+
+//Bucles
+//While (condicion){instrucciones}
+//For (punto de inicio =x; hasta donde quiero llegar; iterador paso++){instrucciones}
+//
+
+
+
+
+
+
